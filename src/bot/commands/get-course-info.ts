adbfe19b8afd71a3f2f008c45e5efe4cf8a7b669
Jordan <122948511+laptomp@users.noreply.github.com>
2026-07-17 16:54:22 -0400

    Feature Discord Embed Implementation (#5)
    Add discord embed responses to the course search command

--- src/bot/commands/get-course-info.ts
+++ src/bot/commands/get-course-info.ts
@@ -1,4 +1,9 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import {
+	AttachmentBuilder,
+	ChatInputCommandInteraction,
+	EmbedBuilder,
+	SlashCommandBuilder,
+} from "discord.js";
 import { getCourse } from "../../api";
 import { Course, CourseSection } from "../../types";
 import { sortSections } from "../../utils";
@@ -23,9 +28,9 @@ module.exports = {
 			return;
 		}
 
-		let content = `Searching for \`${givenCourseCode}\`...`;
+		let descriptionContent = `Searching for \`${givenCourseCode}\`...`;
 
-		await userCommand.reply(content);
+		await userCommand.reply(descriptionContent);
 
 		const course: Course = await getCourse(givenCourseCode.toUpperCase(), [
 			"ARTSC",
@@ -33,9 +38,7 @@ module.exports = {
 			"ERIN",
 		]);
 
-		content = `# ${course.code}: ${course.name}`;
-		content += ` @ ${course.campus === "St. George" ? "UTSG" : course.campus === "Scarborough" ? "UTSC" : "UTM"}`;
-		content += `\n-# This course is offered in the ${course.sectionCode === "F" ? "Fall" : "Winter"}`;
+		descriptionContent = `This course is offered in the ${course.sectionCode === "F" ? "Fall" : "Winter"}`;
 
 		course.sections = sortSections(course.sections);
 
@@ -47,14 +50,25 @@ module.exports = {
 			{},
 		);
 
-		content += ` with ${sectionsCount["LEC"]} Lecture${sectionsCount["LEC"] !== 1 ? "s" : ""},`;
-		content += ` ${sectionsCount["PRAC"] ?? 0} Practical${sectionsCount["PRAC"] !== 1 ? "s" : ""},`;
-		content += ` and ${sectionsCount["TUT"] ?? 0} Tutorial${sectionsCount["TUT"] !== 1 ? "s" : ""}\n`;
+		descriptionContent += ` with ${sectionsCount["LEC"]} Lecture${sectionsCount["LEC"] !== 1 ? "s" : ""},`;
+		descriptionContent += ` ${sectionsCount["PRAC"] ?? 0} Practical${sectionsCount["PRAC"] !== 1 ? "s" : ""},`;
+		descriptionContent += ` and ${sectionsCount["TUT"] ?? 0} Tutorial${sectionsCount["TUT"] !== 1 ? "s" : ""}\n`;
+
+		const fieldContents: Record<string, { name: string; value: string; inline: boolean }> = {
+			LEC: { name: "Lectures 📚", value: "", inline: false },
+			TUT: { name: "Tutorials ✍️", value: "", inline: false },
+			PRA: { name: "Practicals 🔬", value: "", inline: false },
+		};
+
+		const fieldTracker: Record<string, string> = {
+			LEC: "LEC",
+			TUT: "TUT",
+			PRA: "PRA",
+		};
 
 		course.sections.map((section: CourseSection) => {
 			const spots = section.maxEnrolment - section.currentEnrolment;
-			content += "\n";
-			content += (() => {
+			let addingString = (() => {
 				const percentage = (spots / section.maxEnrolment) * 100;
 				switch (true) {
 					case percentage === 0:
@@ -67,13 +81,47 @@ module.exports = {
 						return "🟩";
 				}
 			})();
-			content += ` **${section.name}**`;
-			content += ` with ${spots} spots remaining`;
+			addingString += ` **${section.name}**`;
+			addingString += ` with ${spots}/${section.maxEnrolment} spots remaining`;
 			if (section.currentWaitlist > 0) {
-				content += ` and a waitlist of ${section.currentWaitlist}`;
+				addingString = ` and a waitlist of ${section.currentWaitlist}`;
 			}
+
+			const currentFieldString = fieldContents[fieldTracker[section.teachMethod]]["value"];
+			if (currentFieldString.length + addingString.length + 1 > 1024) {
+				fieldTracker[section.teachMethod] = fieldTracker[section.teachMethod] + "+";
+				fieldContents[fieldTracker[section.teachMethod]] = {
+					name: "",
+					value: addingString + "\n",
+					inline: false,
+				};
+				return;
+			}
+			fieldContents[fieldTracker[section.teachMethod]]["value"] += addingString + "\n";
 		});
 
-		await userCommand.editReply(content);
+		const embedFields = Object.entries(fieldContents)
+			.filter(([teachMethod, field]) => {
+				return field["value"];
+			})
+			.map(([teachMethod, field]) => {
+				return field;
+			});
+
+		const image = new AttachmentBuilder("./src/bot/assets/UofTlogo.png", {
+			name: "jpg.jpg", // will be sent as jpg.jpg
+		});
+
+		const embedResponse = new EmbedBuilder()
+			.setTitle(`${course.code}: ${course.name}`)
+			.setDescription(descriptionContent)
+			.addFields(embedFields)
+			.setFooter({ text: `Course ID: ${course.id}` })
+			.setAuthor({
+				name: "From the University of Toronto Timetable Builder",
+				iconURL: "attachment://jpg.jpg",
+			});
+
+		await userCommand.editReply({ embeds: [embedResponse], files: [image] });
 	},
 };
